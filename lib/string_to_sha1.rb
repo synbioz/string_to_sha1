7c0f1ab150bae50b7148ef9f3b427897b1ed9010
Nicolas Cavigneaux <nico@bounga.org>
2012-04-24 11:47:08 +0200

    Add some documentation

--- lib/string_to_sha1.rb
+++ lib/string_to_sha1.rb
@@ -2,6 +2,9 @@ require "string_to_sha1/version"
 require "digest/sha1"
 
 class String
+  # Convert string to its SHA1 representation
+  #
+  # @return [String] SHA1 of string
   def to_sha1
     Digest::SHA1.hexdigest(self)
   end
